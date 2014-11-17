47db766834f3fb5a28a63f32092958c5c85266d0
tsergium <tsergium@gmail.com>
2014-11-17 18:35:00 +0200

    Homepage and database optimization

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,11 +6,6 @@ class HomeController extends \BaseController {
 	public function index()
 	{
         $pageName = 'Articles List';
-//        $result = DB::table('articles')
-//            ->join('video', 'users.id', '=', 'contacts.user_id')
-//            ->select('users.id', 'contacts.phone', 'orders.price')
-//            ->where()
-//            ->get();
         $results = Articles::orderBy('added', 'desc')->remember(10)->paginate(5);
         View::share('seo_title', $pageName);
         return View::make('home.index', array('articles' => $results, 'pageName' => $pageName));
--- app/views/partials/article.php
+++ app/views/partials/article.php
@@ -1,36 +1,33 @@
 <div class="panel panel-default">
     <div class="panel-heading"><h2><a href="/article/<?php echo $article->id ?>"><?php echo $article->name ?></a></h2></div>
     <div class="panel-body nopadding">
-        <?php if($article->type == 'embed'):?>
+        <?php if($article->type == 'embed' || $article->type == 'video'):?>
             <?php
-            $videoResult = Video::where('product_id', $article->id)->first();
-            if(NULL != $videoResult) {
-                if($videoResult->image)
+            if($article->image)
+            {
+                echo "<img class='img-responsive' src='http://".$_SERVER['SERVER_NAME']."/media/catalog/video/big/" . $article->video_image."' alt='".$article->name."' title='".$article->name."' />";
+            }
+            elseif(preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $article->video_url, $match))
+            { // youtube thumb
+                $video_id = $match[1];
+                $yt_image_qualities = array('maxresdefault', 'hqdefault', 'mqdefault');
+                foreach($yt_image_qualities as $image_quality)
                 {
-                    echo "<img class='img-responsive' src='http://".$_SERVER['SERVER_NAME']."/media/catalog/video/big/" . $videoResult->image."' alt='".$article->name."' title='".$article->name."' />";
-                }
-                elseif(preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $videoResult->url, $match))
-                { // youtube thumb
-                    $video_id = $match[1];
-                    $yt_image_qualities = array('maxresdefault', 'hqdefault', 'mqdefault');
-                    foreach($yt_image_qualities as $image_quality)
+                    if(@getimagesize( ('http://i.ytimg.com/vi/'. $match[1]. '/'.$image_quality.'.jpg') ) )
                     {
-                        if(@getimagesize( ('http://i.ytimg.com/vi/'. $match[1]. '/'.$image_quality.'.jpg') ) )
-                        {
-                            echo "<i><img data-youtube-id='$match[1]' class='img-responsive js-lazy-youtube' src='http://i.ytimg.com/vi/$video_id/$image_quality.jpg' alt='".$article->name."' title='".$article->name."' /></i>";
-                            break; //exiting
-                        }
+                        echo "<i><img data-youtube-id='$match[1]' class='img-responsive js-lazy-youtube' src='http://i.ytimg.com/vi/$video_id/$image_quality.jpg' alt='".$article->name."' title='".$article->name."' /></i>";
+                        break; //exiting
                     }
                 }
-                else
-                { // vimeo thumb
-                    sscanf(parse_url($url, PHP_URL_PATH), '/%d', $video_id);
-                    $hash = unserialize(file_get_contents('http://vimeo.com/api/v2/video/'. $video_id .'.php'));
-                    $hqUrl = $hash[0]['thumbnail_large'];
-                    $hqtumb = "<img class='img-responsive' src='".$hqUrl."' alt='%s' title='%s' />";
-                    $html = "";
-                    $html .= sprintf($hqtumb, $name, $name);
-                }
+            }
+            else
+            { // vimeo thumb
+                sscanf(parse_url($url, PHP_URL_PATH), '/%d', $video_id);
+                $hash = unserialize(file_get_contents('http://vimeo.com/api/v2/video/'. $video_id .'.php'));
+                $hqUrl = $hash[0]['thumbnail_large'];
+                $hqtumb = "<img class='img-responsive' src='".$hqUrl."' alt='%s' title='%s' />";
+                $html = "";
+                $html .= sprintf($hqtumb, $name, $name);
             }
         ?>
         <?php endif ?>
