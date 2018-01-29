02bdefccca821ee62794869e861e4f28b9622607
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2018-01-29 18:48:41 +0100

    Fix product creation layout

--- app/views/product/create.blade.php
+++ app/views/product/create.blade.php
@@ -1,49 +1,51 @@
 @extends('layouts.master')
 @section('content')
+<div class="container">
+    <div class="row">
+        <h1>Create a Nerd</h1>
 
-<h1>Create a Nerd</h1>
+        {{ HTML::ul($errors->all()) }}
 
-<!-- if there are creation errors, they will show here -->
-{{ HTML::ul($errors->all()) }}
+        {{ Form::open(array('url' => 'product/store')) }}
 
-{{ Form::open(array('url' => 'product/store')) }}
+        <div class="form-group">
+            {{ Form::label('name', 'Name') }}
+            {{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
+        </div>
 
-    <div class="form-group">
-        {{ Form::label('name', 'Name') }}
-        {{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
+        <div class="form-group">
+            {{ Form::label('oldPrice', 'Old Price') }}
+            {{ Form::text('oldPrice', Input::old('oldPrice'), array('class' => 'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('newPrice', 'New Price') }}
+            {{ Form::text('newPrice', Input::old('newPrice'), array('class' => 'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('description', 'Description') }}
+            {{ Form::text('description', Input::old('description'), array('class' => 'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('url', 'URL') }}
+            {{ Form::text('url', Input::old('url'), array('class' => 'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('affiliateUrl', 'Affiliate URL') }}
+            {{ Form::text('affiliateUrl', Input::old('affiliateUrl'), array('class' => 'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('image', 'Image') }}
+            {{ Form::text('image', Input::old('image'), array('class' => 'form-control')) }}
+        </div>
+
+        {{ Form::submit('Create the Product!', array('class' => 'btn btn-primary')) }}
+
+        {{ Form::close() }}
     </div>
-
-    <div class="form-group">
-        {{ Form::label('oldPrice', 'Old Price') }}
-        {{ Form::text('oldPrice', Input::old('oldPrice'), array('class' => 'form-control')) }}
-    </div>
-
-    <div class="form-group">
-        {{ Form::label('newPrice', 'New Price') }}
-        {{ Form::text('newPrice', Input::old('newPrice'), array('class' => 'form-control')) }}
-    </div>
-
-    <div class="form-group">
-        {{ Form::label('description', 'Description') }}
-        {{ Form::text('description', Input::old('description'), array('class' => 'form-control')) }}
-    </div>
-
-    <div class="form-group">
-        {{ Form::label('url', 'URL') }}
-        {{ Form::text('url', Input::old('url'), array('class' => 'form-control')) }}
-    </div>
-
-    <div class="form-group">
-        {{ Form::label('affiliateUrl', 'Affiliate URL') }}
-        {{ Form::text('affiliateUrl', Input::old('affiliateUrl'), array('class' => 'form-control')) }}
-    </div>
-
-    <div class="form-group">
-        {{ Form::label('image', 'Image') }}
-        {{ Form::text('image', Input::old('image'), array('class' => 'form-control')) }}
-    </div>
-
-    {{ Form::submit('Create the Product!', array('class' => 'btn btn-primary')) }}
-
-{{ Form::close() }}
+</div>
 @stop
