a1fa5f3a2d083bc86fa12c2a9312b9b65317752f
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2018-01-29 19:14:37 +0100

    Adding dynamic image

--- app/views/partials/product.blade.php
+++ app/views/partials/product.blade.php
@@ -1,7 +1,7 @@
 <div class="col-sm-3 dynpost">
     <div class="modul-produs">
         <div class="rebon1"><p>50%</p></div>
-        <img src="img/PluginzShoot2-21_1024x1024.jpg" class="img-responsive image-product">
+        <img src="{{ $product->image }}" class="img-responsive image-product">
         <div class="descriere">
             <h1>{{ $product->name }}</h1>
             <p><span>{{ number_format($product->oldPrice, 2) }}</span> / {{ number_format($product->newPrice, 2) }} lei</p>
