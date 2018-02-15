4c9647c987f0e0ddf56f1b3df40aa41885ec1d94
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2018-02-15 11:12:49 +0100

    Improved link

--- app/views/partials/product.blade.php
+++ app/views/partials/product.blade.php
@@ -3,13 +3,13 @@
         <div class="rebon1"><p>50%</p></div>
         <a href="{{ $product->affiliateUrl }}" title="{{ $product->name }}">
             <img src="{{ $product->image }}" class="img-responsive image-product" alt="{{ $product->name }}">
+            <div class="descriere">
+                <h1>{{ $product->name }}</h1>
+                <p><span>{{ number_format($product->oldPrice, 2) }}</span> / {{ number_format($product->newPrice, 2) }} lei</p>
+                @if (Auth::check())
+                    <a class="btn btn-default btn-xs" href="{{ URL::to('product/' . $product->id . '/edit') }}">edit</a>
+                @endif
+            </div>
         </a>
-        <div class="descriere">
-            <h1>{{ $product->name }}</h1>
-            <p><span>{{ number_format($product->oldPrice, 2) }}</span> / {{ number_format($product->newPrice, 2) }} lei</p>
-            @if (Auth::check())
-                <a class="btn btn-default btn-xs" href="{{ URL::to('product/' . $product->id . '/edit') }}">edit</a>
-            @endif
-        </div>
     </div>
 </div>
